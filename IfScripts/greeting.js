b08d0efae8e312ceabd669317dd36de31427ab0e
Zhayida Haishan <zahideqeyser01@gmail.com>
2023-04-28 10:26:04 -0700

    update

---
+++ IfScripts/greeting.js
@@ -0,0 +1,39 @@
+/*
+
+    Author: Zhayida Haishan
+
+    Description: this script detects time and depending on the time, 
+    it outputs message that falls into that timeline.
+
+*/
+
+// ------- Greeting ------- //
+
+"use sprict";
+
+// input
+let currentTime = 10;
+let message;
+
+// calculation
+if ( currentTime < 11 ){
+    message = "Good Morning!"
+}
+else if ( currentTime < 17 ){
+    message = "Good Day!"
+}
+else {
+    message = "Good Evening!"
+}
+    
+// output
+console.log( message );
+
+// Running
+/*
+
+    until 10am >> Good Morning!
+    10 - 16.59 >> Good Day!
+    17 and later >> Good Evening!
+
+*/
