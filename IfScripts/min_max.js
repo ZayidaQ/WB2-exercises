cb86fa09eee0cbda3553be719f02975dfeff5d9f
Zhayida Haishan <zahideqeyser01@gmail.com>
2023-05-10 22:16:20 -0700

    Create min_max.js

---
+++ IfScripts/min_max.js
@@ -0,0 +1,31 @@
+/*
+
+    Author: Zhayida Haishan
+
+    Description: this JS displays both the smallest of the three
+    numbers and then the  largest of three numbers.
+
+*/
+
+// ------- Min / Max displayer ------- //
+
+"use sprict";
+
+// input
+let a = 5;
+let b = 10;
+let c = 15;
+
+let maxValue;
+
+
+
+
+// calculation
+if ( outputValue == "Max" ){
+    console.log(a)
+}
+else 
+
+// output
+console.log()
